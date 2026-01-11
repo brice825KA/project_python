b5ab8e1751d4ef85951b5d948e9703918d035a89
brice.adomahou <brice825KA@users.noreply.github.com>
2026-01-11 16:51:31 +0100

    first commit

--- count_occ.py
+++ count_occ.py
@@ -1,3 +1,6 @@
 def count_occ(sentence="brice", char='c'):
-    sent = sentence + ' ' + char
-    return sent
+    count = 0
+    for i in range(1, len(sentence)):
+        if sentence[i] == char:
+            count += 1
+    return count
