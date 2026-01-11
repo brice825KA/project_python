7542ef84123010078182899ec9268520740666e8
brice.adomahou <brice825KA@users.noreply.github.com>
2026-01-11 07:50:24 +0100

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
 from FizzBuzz import fizzbuzz
+from table_mul import multiple_table
 
 def main():
     if len(sys.argv) == 1:
         return 84
     else:
-        fizzbuzz(int(sys.argv[1]))
+        multiple_table(int(sys.argv[1]))
 if __name__ == "__main__":
     main()
